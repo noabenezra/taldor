10fdba0e7cf1c78be214b16e3df163333212b59c
noa <noabenezra@gmail.com>
2018-05-24 12:01:52 +0300

    1

--- website/FlickrPic.js
+++ website/FlickrPic.js
@@ -1,35 +1,16 @@
 'use strict';
-
-
 angular.module('myApp', ['ngMaterial'])
-
   .controller('AppCtrl', function ($scope, $mdDialog) {
-    debugger;
     $scope.status = '  ';
     $scope.customFullscreen = false;
-    $scope.apiFlicker = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&user_id=141840526@N02&format=json&nojsoncallback=1&per_page=500";
-
-    /*$(document).ready(function () {
-      debugger;
-      $("md-button").click(function () {
-        debugger;
-        $.getJSON(apiFlicker, function (result) {
-          debugger;
-          $.each(result, function (i, photo) {
-            var photoList = photo.photo;
-          });
-        });
-      });
-    });*/
-
     $scope.showAdvanced = function (ev) {
       $mdDialog.show({
         controller: DialogController,
-        templateUrl: 'Dialog.html',
+        template: '<div class="dialog"><div class="search"><input class="search-input" type="text" ng-model="query" placeholder="Search for names.." title="Type in a name"></div><div><span ng-repeat="photo in photoList | filter:query"><span><a href="{{photo.url}}"><img src={{photo.url}} title={{photo.title}}/></a></span></span></div><div><md-button class="button" ng-click="cancel()">Exit</md-button></div></div>',
         parent: angular.element(document.body),
         targetEvent: ev,
-        clickOutsideToClose: true,
-        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+        clickOutsideToClose: false,
+        fullscreen: $scope.customFullscreen
       })
         .then(function (answer) {
           $scope.status = 'You said the information was "' + answer + '".';
@@ -37,18 +18,15 @@ angular.module('myApp', ['ngMaterial'])
           $scope.status = 'You cancelled the dialog.';
         });
     };
-
     function DialogController($scope, $mdDialog) {
+      $scope.apiFlicker = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&user_id=141840526@N02&format=json&nojsoncallback=1&per_page=500";
 
-      $.getJSON(apiFlicker, function (result) {
-        debugger;
-        $.each(result, function (i, photo) {
-          debugger;
-          $scope.photoList = photo.photo;
+      $.getJSON($scope.apiFlicker, function (result) {
+        $scope.photoList = result.photos.photo;
+        $scope.photoList.forEach(function (element) {
+          element.url = "https://farm1.staticflickr.com/" + element.server + "/" + element.id + "_" + element.secret + "_m.jpg";
         });
       });
-
-
 
       $scope.hide = function () {
         $mdDialog.hide();
@@ -61,5 +39,6 @@ angular.module('myApp', ['ngMaterial'])
       $scope.answer = function (answer) {
         $mdDialog.hide(answer);
       };
+      
     }
   });
